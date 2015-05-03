1278449d9f237268a2c9c89984e9fc3ff1067b41
tibus29 <thibault.pensec@gmail.com>
2015-05-03 16:32:41 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tibus on 03/05/15.
+ */
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['page-1', 'page-2', 'page-3', 'page-4'],
+        menu: '#mainMenu'
+    });
+});
