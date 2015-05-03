6873bfef17b0b2b89e38d0d996f52597b6e71a0c
tibus29 <thibault.pensec@gmail.com>
2015-05-03 19:05:56 +0200

    rename FullPage.js sections

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
  */
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['page-1', 'page-2', 'page-3', 'page-4'],
+        anchors: ['home', 'skills', 'portfolio', 'contact'],
         menu: '#mainMenu'
     });
 });
